74f6b1a85929d0c5bb14945921feb51c198a1a29
Thomas Patterson <tom.patterson@iventis.co.uk>
2020-10-07 22:11:22 +0100

    Restructured and added comments to UCI communication method so it is more maintable

--- Dionysus/dionysus.cpp
+++ Dionysus/dionysus.cpp
@@ -20,22 +20,35 @@ void process_UCI() {
 
 	bool first_search = true;
 
+	//read each instruction
 	while (std::getline(std::cin, instruction)) {
+
+		//command = first word in each instruction
 		command = instruction.substr(0, instruction.find(' '));
+
+		//pos keeps track of current position while parsing each instruction
 		int pos = command.size() + 1;
+
+		//respond to uci with name and author
 		if (command == "uci") {
 			std::cout << "id name Dionysus \nid author Thomas Patterson\n";
 			std::cout << "uciok" << std::endl;
 		}
+		//respond to isready with readyok, as per spec
 		else if (command == "isready") {
 			std::cout << "readyok" << std::endl;
 		}
+		//position specifies current board position
 		else if (command == "position") {
 			std::string type = instruction.substr(pos, instruction.find(' ', pos) - pos);
 			pos += type.size() + 1;
+
+			//if startpos, create default board
 			if (type == "startpos") {
 				board = Board();
 			}
+
+			//else if fen, read in the fen, and create board with that fen
 			else if (type == "fen") {
 				std::string fen = "";
 				for (int i = 0; i < 6; i++) {
@@ -45,7 +58,11 @@ void process_UCI() {
 				}
 				board = Board(fen);
 			}
+
+			//if no more moves are specified, move to next instruction
 			if (pos >= instruction.size()) continue;
+
+			//otherwise read in and make the specified moves
 			std::string move = instruction.substr(pos, instruction.find(' ', pos) - pos);
 			pos += move.size() + 1;
 			if (move == "moves") {
@@ -57,21 +74,34 @@ void process_UCI() {
 				}
 			}
 		}
+
+		//when recieve go, start searching on currently loaded position
 		else if (command == "go") {
+
+			//need to join thread from previous search before we can start this one
 			if (first_search) first_search = false;
 			else searching_thread.join();
+
 			searching_thread = std::thread(perform_search);
 		}
+
+		//stop indicates we should stop searching
 		else if (command == "stop") {
 			searcher.stop();
 		}
+
+		//quit indicates we should end program
 		else if (command == "quit") {
 			break;
 		}
+
+		//if unrecognised command
 		else {
 			std::cout << "QUE??" << std::endl;
 		}
 	}
+	
+	//when exiting program
 	searching_thread.detach();
 }
 
